4747afb999bcc98925966c51ab4df89c19c3effb
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-21 15:18:49 -0500

    :sparkles: config environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from "@nestjs/config";
 import { join } from "path";
 import { Module } from "@nestjs/common";
 import { ServeStaticModule } from "@nestjs/serve-static";
@@ -7,13 +8,18 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "public"),
     }),
-    MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon"),
+    MongooseModule.forRoot(process.env.DB_HOST),
     PokemonModule,
     CommonModule,
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
