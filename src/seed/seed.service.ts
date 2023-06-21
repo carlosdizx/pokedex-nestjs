60cb231560d31d9a1d169f93245c5f3f7238d1a7
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-20 22:14:22 -0500

    :sparkles: consume api and returned

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import axios, { AxiosInstance } from "axios";
 
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
   public executeSeed = async () => {
-    return { message: "Seed execute" };
+    const { data } = await this.axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=650"
+    );
+    return data;
   };
 }
